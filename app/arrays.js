bd5b9955c94dc219b1575776395b71e8ac55cbfd
Alex Paczynski <apaczy@gmail.com>
2016-04-13 13:50:38 -0700

    Completed through arrays.js removeWithoutCopy, working on append

--- app/arrays.js
+++ app/arrays.js
@@ -3,23 +3,45 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    var index;
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        index = i;
+      }
+    }
+    return index || -1;
   },
 
   sum : function(arr) {
-
+    var total = 0;
+    _.each(arr, function (item) {
+      total += item;
+    });
+    return total;
   },
 
   remove : function(arr, item) {
-
+    var newArr = [];
+    _.each(arr, function (value) {
+      if(value !== item){
+        newArr.push(value);
+      }
+    });
+    return newArr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    
   },
 
   truncate : function(arr) {
